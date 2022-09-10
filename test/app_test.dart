4bb1d3b85399074cadda92041aa4960ef1e18b4a
roaa94 <roaa94khaddam@gmail.com>
2022-09-11 02:34:01 +0300

    chore: adds key to App test for coverage

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:widgetbook_challenge/app.dart';
 import 'package:widgetbook_challenge/form/views/pages/form_page.dart';
 
 void main() {
   testWidgets('$App contains FormPage', (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(App(key: UniqueKey()));
 
     expect(find.byType(FormPage), findsOneWidget);
   });
