eb77c3c77f48e84f6b8ba98678f5bd0f065523b4
roaa94 <roaa94khaddam@gmail.com>
2022-09-11 01:15:01 +0300

    chore: adds form submission logic in most basic code

--- lib/form_page.dart
+++ lib/form_page.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
+/// Helper enum for status of the request
+enum RequestStatus {
+  /// The request is loading
+  loading,
+
+  /// The request returned an error
+  error,
+
+  /// The request was successful
+  success,
+
+  /// The request has not been sent yet
+  idle,
+}
+
 /// The Form Page
 class FormPage extends StatefulWidget {
   /// Creates instance of the Form page
@@ -10,6 +25,34 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  String? name;
+  RequestStatus requestStatus = RequestStatus.success;
+  String message = '';
+
+  Future<void> submit() async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+    _formKey.currentState!.save();
+    setState(() {
+      requestStatus = RequestStatus.loading;
+      message = '';
+    });
+    try {
+      await Future<dynamic>.delayed(const Duration(seconds: 1));
+      setState(() {
+        requestStatus = RequestStatus.success;
+        message = 'Submitted successfully!';
+      });
+    } catch (e) {
+      setState(() {
+        requestStatus = RequestStatus.error;
+        message = e.toString();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,14 +61,44 @@ class _FormPageState extends State<FormPage> {
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            TextFormField(
-              onSaved: (value) {
-                //...
-              },
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                onSaved: (value) {
+                  name = value;
+                },
+                decoration: const InputDecoration(
+                  hintText: 'Name',
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Name Field is required';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed:
+                    requestStatus == RequestStatus.loading ? null : submit,
+                child: requestStatus == RequestStatus.loading
+                    ? const CircularProgressIndicator.adaptive()
+                    : const Text('Submit'),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                message,
+                style: TextStyle(
+                  color: requestStatus == RequestStatus.error
+                      ? Colors.red
+                      : Colors.green,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
